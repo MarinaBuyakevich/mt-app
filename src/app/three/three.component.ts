8581963412610cff445167b2fd5d0dc6496dd3af
MarinaBuyakevich <32779490+MarinaBuyakevich@users.noreply.github.com>
2018-12-10 11:34:03 +0200

    Added export class

--- src/app/three/three.component.ts
+++ src/app/three/three.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./three.component.css']
 })
 export class ThreeComponent implements OnInit {
-
+  text = 'The Angular CLI makes it easy to create an application that already works, right out of the box.';
   constructor() { }
 
   ngOnInit() {
