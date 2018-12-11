0001bdb8d6efa034aac787a1ca9794b2a09f48c1
MarinaBuyakevich <32779490+MarinaBuyakevich@users.noreply.github.com>
2018-12-11 15:12:21 +0200

    new file

---
+++ src/app/config.ts
@@ -0,0 +1,13 @@
+export function config() {return {
+  app: {
+    name_full: 'BANDA tech',
+    name_1: 'BandA',
+    name_2: 'TECH',
+    default_lang: 'EN',
+    lang_list: ['EN', 'RU']
+  },
+  dev: {
+    mode: 1
+  }
+};
+}
