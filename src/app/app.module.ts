4c99130cf4f2e83bbcef364b037b545f877409b9
MarinaBuyakevich <32779490+MarinaBuyakevich@users.noreply.github.com>
2018-12-11 15:10:36 +0200

    imported new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentnameComponent } from './componentname/componentname.component';
 import { ThreeComponent } from './three/three.component';
+import { MenuComponent } from './menu/menu.component';
+import { MajorPrioritiesComponent } from './major-priorities/major-priorities.component';
+import { PrioritiesComponent } from './priorities/priorities.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
+import { TranslatorService } from './services/translator.service';
 @NgModule({
   declarations: [
     AppComponent,
     ComponentnameComponent,
-    ThreeComponent
+    ThreeComponent,
+    MenuComponent,
+    MajorPrioritiesComponent,
+    PrioritiesComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    TranslatorService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
