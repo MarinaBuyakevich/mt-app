a9f9cbd5aa5ef4f754e3dc91d5a0d2db2e8f7fb4
MarinaBuyakevich <my email address used on my git repo>
2018-12-12 17:48:48 +0200

    code changed

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {TranslatorService} from '../services/translator.service';
+import {config} from '../config';
+
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  // private changeLang: Subscription;
+  // public source: any;
+  // private menu: any;
+  public menuLinks: any;
+  public activeLink: string;
+  public config: any;
+  constructor(public ts: TranslatorService) {
+    this.menuLinks = [];
+    this.menuLinks.push('home');
+    this.menuLinks.push('priorities');
+    this.menuLinks.push('techniques');
+    this.menuLinks.push('contact');
+    this.activeLink = this.menuLinks[0];
+    this.config = config().app;
+}
+  select (ev: Event, ln: string) {
+    ev.preventDefault();
+    setTimeout(() => {
+      const target = document.getElementById(ln);
+      target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }, 30);
+    this.activeLink = ln;
   }
 
+  ngOnInit(): void {
+  }
 }
