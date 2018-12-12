5affabd224e14232adeb5505953da568eda60309
MarinaBuyakevich <my email address used on my git repo>
2018-12-12 17:49:53 +0200

    code changed

--- src/app/three/three.component.ts
+++ src/app/three/three.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {TranslatorService} from '../services/translator.service';
+import {three} from '../dictionaries/three';
+import {Subscription} from 'rxjs';
+
 
 @Component({
   selector: 'app-three',
@@ -7,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThreeComponent implements OnInit {
   text = 'The Angular CLI makes it easy to create an application that already works, right out of the box.';
-  ts: any;
-  constructor() { }
+  private changeLang: Subscription;
+  public source: string;
+  private three: any;
+  constructor(public ts: TranslatorService) {
+    this.changeLang = ts.changeLang$.subscribe(data => {
+      this.setSource(data.lang);
+    });
+    this.source = this.three[ts.currentLang];
+  }
 
   ngOnInit() {
   }
 
+  private setSource(lang: string) {
+    this.source = three[lang];
+  }
 }
