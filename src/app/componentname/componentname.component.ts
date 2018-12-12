8b7bb7f58bd06357b7d2ebde10d361a799af197f
MarinaBuyakevich <my email address used on my git repo>
2018-12-12 17:38:13 +0200

    code changed

--- src/app/componentname/componentname.component.ts
+++ src/app/componentname/componentname.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {TranslatorService} from '../services/translator.service';
+import {componentname} from '../dictionaries/componentname';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-componentname',
@@ -6,11 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./componentname.component.css']
 })
 export class ComponentnameComponent implements OnInit {
-  ts: any;
-
-  constructor() { }
+  private changeLang: Subscription;
+  public source: any;
+  private componentname: any;
+  constructor(public ts: TranslatorService) {
+    this.changeLang = ts.changeLang$.subscribe(data => {
+      this.setSource(data.lang);
+    });
+    this.source = this.componentname[ts.currentLang];
+  }
 
   ngOnInit() {
   }
 
+  private setSource(lang: string) {
+    this.source = componentname[lang];
+  }
 }
