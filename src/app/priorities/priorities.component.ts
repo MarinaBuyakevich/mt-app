6ca71690f516d8c21c96decdc02040775c22865d
MarinaBuyakevich <my email address used on my git repo>
2018-12-12 17:49:18 +0200

    code changed

--- src/app/priorities/priorities.component.ts
+++ src/app/priorities/priorities.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {TranslatorService} from '../services/translator.service';
+import {priorities} from '../dictionaries/priorities';
+import {Subscription} from 'rxjs';
+
 
 @Component({
   selector: 'app-priorities',
@@ -6,11 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./priorities.component.css']
 })
 export class PrioritiesComponent implements OnInit {
-  ts: any;
-
-  constructor() { }
+  private changeLang: Subscription;
+  public source: any;
+  private priorities: any;
+  constructor(public ts: TranslatorService) {
+    this.changeLang = ts.changeLang$.subscribe(data => {
+      this.setSource(data.lang);
+    });
+    this.source = this.priorities[ts.currentLang];
+  }
 
   ngOnInit() {
   }
 
+  private setSource(lang: string) {
+    this.source = priorities[lang];
+  }
 }
