381b8d5f460b962b66caa4ba2f2a0b1fed4e28ea
MarinaBuyakevich <my email address used on my git repo>
2018-12-13 17:37:00 +0200

    code changed

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {TranslatorService} from '../services/translator.service';
-import {footer} from '../dictionaries/footer';
+import {Footer} from '../dictionaries/footer';
 import {Subscription} from 'rxjs';
 
 @Component({
@@ -11,18 +11,17 @@ import {Subscription} from 'rxjs';
 export class FooterComponent implements OnInit {
   private changeLang: Subscription;
   public source: string;
-  private footer: any;
   constructor(public ts: TranslatorService) {
     this.changeLang = ts.changeLang$.subscribe(data => {
       this.setSource(data.lang);
     });
-    this.source = this.footer[ts.currentLang];
+    this.source = Footer[this.ts.currentLang];
   }
 
   ngOnInit() {
   }
 
   private setSource(lang: string) {
-    this.source = footer[lang];
+    this.source = Footer[lang];
   }
 }
