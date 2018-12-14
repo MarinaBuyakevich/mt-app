182b5537b66190510be88c0f50ba080d8df1519d
MarinaBuyakevich <my email address used on my git repo>
2018-12-14 17:26:29 +0200

    code is changed

--- src/app/major-priorities/major-priorities.component.ts
+++ src/app/major-priorities/major-priorities.component.ts
@@ -1,31 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewChecked, Component, OnInit} from '@angular/core';
 import {TranslatorService} from '../services/translator.service';
-import {MPriorities} from '../dictionaries/major-priorities';
+import {trigger} from '@angular/animations';
 import {Subscription} from 'rxjs';
+import {MPriorities} from '../dictionaries/major-priorities';
+import {config} from '../config';
 
 @Component({
   selector: 'app-major-priorities',
   templateUrl: './major-priorities.component.html',
-  styleUrls: ['./major-priorities.component.css']
+  styleUrls: ['./major-priorities.component.css'],
+  styles: [``],
+  animations: [
+    trigger('mat-checkbox', [
+
+    ]),
+  ]
 })
-export class MajorPrioritiesComponent implements OnInit {
-  private changeLang: Subscription;
+export class MajorPrioritiesComponent implements OnInit, AfterViewChecked {
+  public move: boolean;
   public langList: any;
   public activeLang: string;
   public source: any;
-  constructor(public ts: TranslatorService) {
+  private changeLang = Subscription;
+
+  constructor(
+    public ts: TranslatorService) {
+    this.move = false;
+    this.langList = config().app.lang_list;
+    this.langList = ts.langList;
+    this.activeLang = 'EN';
     this.changeLang = ts.changeLang$.subscribe(data => {
-      this.setSource(data.lang);
+      this.setSource(data['lang']);
     });
     this.source = MPriorities[this.ts.currentLang];
   }
 
   ngOnInit() {
   }
-
-  // public setSource(lang: string) {
-  //   this.source = MPriorities[lang];
-  // }
 
   setLang(ev: Event, lang: string) {
     ev.preventDefault();
@@ -35,6 +46,16 @@ export class MajorPrioritiesComponent implements OnInit {
   setSource(lang: string) {
     this.source = MPriorities[lang];
   }
-}
 
+  ngAfterViewChecked() {
+    const startFrame = document.getElementById('start-frame');
+    if (startFrame.clientHeight !== window.innerHeight) {
+      startFrame.style.height = window.innerHeight + 'px';
+      const items: any = startFrame.querySelectorAll('.graph .graph-wrapper .graph-inner .graph-item');
+      items.forEach(item => {
+        item.lastChild.style.width = startFrame.clientWidth * 2 + 'px';
+      });
+    }
+  }
+}
 
